282886a9be811997aad7854ddd2350db93342318
sudiptpa <sudiptpa@gmail.com>
2014-09-11 17:56:31 +0545

    Controllers aded

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeController extends BaseController {
+class LoginController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
@@ -15,9 +15,9 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
+	public function getIndex()
 	{
-		return "Hello, you are inside home controller";
+		return View::make('hello');
 	}
 
 }
