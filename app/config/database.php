0a2bba0461a1ca4045f552e1a71ceec3d857492e
Sujip Thapa <sudiptpa@gmail.com>
2014-09-23 17:42:35 +0545

    footer proble fixed

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'db_project',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'sudip123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	$(".sidebar-menus ul li a").each(function() {
 	if($(this).attr('href') == url){
 	  $(this).parent().addClass('current');
-	  $(this).append('<span class="glyphicon glyphicon-chevron-right" style="font-size:8px; padding: 1px 3px 10px 30px !important;position: absolute;top: 11px;"></span>');
+	  $(this).append('<span class="glyphicon glyphicon-chevron-right" style="font-size:8px;position: absolute;top: 11px;top: 31%;right: 13px;"></span>');
 	}
 	});
 });
