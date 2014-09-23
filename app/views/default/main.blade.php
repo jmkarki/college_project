0442d2f7768c1441e40e2f60b304b2a4535cc3b6
sudiptpa <sudiptpa@gmail.com>
2014-09-23 06:46:49 +0545

    customer form ready

--- app/views/default/main.blade.php
+++ app/views/default/main.blade.php
@@ -17,14 +17,14 @@
 	<div class="body-content">
 		<div class="container">
 			<div class="row body-content-row">
-				<div class="col-md-3 col-sm-3">
+				<div class="col-md-2 col-sm-3">
 					<div class="sidebar">
 						<div class="profile">							
 							@include('include.sidebar');							
 						</div>
 					</div>	
 				</div>
-				<div class="col-md-9 col-sm-9" style="padding:0;">
+				<div class="col-md-10 col-sm-9" style="padding:0;">
 					<div class="content">
 					@yield('content')
 					</div>
@@ -36,8 +36,6 @@
 </div>
 {{HTML::script('assets/js/jquery.js')}}
 {{HTML::script('assets/js/custom.js')}}
-
-
 @yield('script')
 </body>
 </html>
--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
 		$('.alert-success').fadeOut(1600,"linear",complete);
 		}
 	});
+	var url = window.location;
+	$(".sidebar-menus ul li a").each(function() {
+	if($(this).attr('href') == url){
+	  $(this).parent().addClass('current');
+	  $(this).append('<span class="glyphicon glyphicon-chevron-right" style="font-size:8px; padding: 1px 3px 10px 30px !important;position: absolute;top: 11px;"></span>');
+	}
+	});
 });
+
+
