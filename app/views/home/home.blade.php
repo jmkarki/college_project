d06e05601f0e9a9a74318b4e19387e8763f4d7d4
Sujip Thapa <sudiptpa@gmail.com>
2014-11-10 17:03:52 +0545

    dynamic validation added

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -1,4 +1,26 @@
 @extends('default.main')
 @section('content')
 welcome to home page || and your session id is {{Session::get('company_id')}}
+
+<h3>Dynamic form example.</h3>
+<form class="commentForm">
+    <div id="inputs"></div>
+    <input type="submit" class="btn-green btn-normal" />
+    <span id="addInput" class="btn-green btn-normal">Add Button</span>
+
+</form>
+ @stop
+ @section('script')
+ <script type="text/javascript">
+ 	$(document).ready(function () {
+    
+     var numberIncr = 1;
+    $("#addInput").on('click', function () {
+        $('#inputs').append($('<input class="comment required form-control" style="width:50%; margin:10px;" name="name[' + numberIncr + ']" />'));
+        numberIncr++;
+    });
+
+    $('form.commentForm').validate();
+});
+ </script>
  @stop
