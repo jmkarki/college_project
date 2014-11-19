3a426403a3cb171fe6815bc1b9420e6ed874dc30
aspiredinesh <ag_danish862outlook.com>
2014-11-19 09:42:07 +0545

    bug fixed

--- public/assets/js/product-list.js
+++ public/assets/js/product-list.js
@@ -10,35 +10,35 @@ $(document).ready(function(){
 				$('.product-title').html(response.product_name);
 				$('.product-des').html(response.product_description);
 				$('.product-img').attr('src',response.imgUrl);
+				var result = '';
 				 for (var i = 0; i < response.option.length; i++) {
-				 	var result = '';
-			 		result += '<div class="option-content">'+
-	 					'<div class="row app-row">'+
-							'<div class="col-md-4">'+
-								'<div class="option-title">'+response.option[i].option_name+'</div>'+
-								'<div class="option-price">'+
-									'Cost Price: <strong>'+response.option[i].price.cost_price+'</strong>'+
-								'</div>'+
-								'<div class="option-price">'+
-									'Market Price: <strong>'+response.option[i].price.market_price+'</strong>'+
-								'</div>'+
-								'<div class="option-price">'+
-									'Selling Price: <strong>'+response.option[i].price.sell_price+'</strong>'+
-								'</div>'+
-							'</div>'+
-							'<div class="col-md-8">'+
-								'<div class="option-des">'+response.option[i].option_description+'</div>'+
-							'</div>'+
-						'</div>'+
-						'<div class="row app-row">'+
-							'<div class="table-responsive">'+
-								'<table class="table table-stripped">'+
-									'<tr><th>Lot No</th><th>Batch No</th><th>Manufactured Date</th><th>Purchased Date</th><th>Expiry Date</th></tr>'+
-									'<tr><td>'+response.option[i].price.lot_no+'</td><td>'+response.option[i].price.batch_no+'</td><td>'+response.option[i].price.manufacture_date+'</td><td>'+response.option[i].price.purchase_date+'</td><td>'+response.option[i].price.expiry_date+'</td></tr>'+
-								'</table>'+
-							'</div>'+
-						'</div>'+
-					'</div>';
+				 		result = result + '<div class="option-content">'+
+							 					'<div class="row app-row">'+
+													'<div class="col-md-4">'+
+														'<div class="option-title">'+response.option[i].option_name+'</div>'+
+														'<div class="option-price">'+
+															'Cost Price: <strong>'+response.option[i].price.cost_price+'</strong>'+
+														'</div>'+
+														'<div class="option-price">'+
+															'Market Price: <strong>'+response.option[i].price.market_price+'</strong>'+
+														'</div>'+
+														'<div class="option-price">'+
+															'Selling Price: <strong>'+response.option[i].price.sell_price+'</strong>'+
+														'</div>'+
+													'</div>'+
+													'<div class="col-md-8">'+
+														'<div class="option-des">'+response.option[i].option_description+'</div>'+
+													'</div>'+
+												'</div>'+
+												'<div class="row app-row">'+
+													'<div class="table-responsive">'+
+														'<table class="table table-stripped">'+
+															'<tr><th>Lot No</th><th>Batch No</th><th>Manufactured Date</th><th>Purchased Date</th><th>Expiry Date</th></tr>'+
+															'<tr><td>'+response.option[i].price.lot_no+'</td><td>'+response.option[i].price.batch_no+'</td><td>'+response.option[i].price.manufacture_date+'</td><td>'+response.option[i].price.purchase_date+'</td><td>'+response.option[i].price.expiry_date+'</td></tr>'+
+														'</table>'+
+													'</div>'+
+												'</div>'+
+											'</div>';
 				 }
 				$('.option-content-holder').html(result);
 			}
