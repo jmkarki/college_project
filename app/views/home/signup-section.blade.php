533f7d7b59ae338a9c0d9797541efba65f66ac6e
sudiptpa <sudiptpa@gmail.com>
2014-11-25 18:51:38 +0545

    slider

--- app/views/home/signup-section.blade.php
+++ app/views/home/signup-section.blade.php
@@ -1,4 +1,4 @@
-<div class="body-content">
+<!-- <div class="body-content">
 	<div class="container" style="zoom: 102%; padding-bottom:20px;">		
 		<div class="row">			
 			<div class="col-md-7 col-" style="padding: 20px; color: #666 !important;"> 				
@@ -17,10 +17,10 @@
 	            <strong style="font-size: 20px;line-height: 32px;">Are you an ERP or commerce partner, Powerful collaboration, and product management,reports. We have a business opportunity for you!</strong>
 	        </div>
 			<div class="col-md-5">
-				<form role="form">
+				<form role="form"> -->
 			        <!-- <h3>Please Sign Up <small>It's free and always will be.</small></h3> -->
 			        <!-- <hr class="colorgraph"> -->
-			        <div class="row" >
+			    <!--     <div class="row" >
 			          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 row-margin-right">
 			            <div class="form-group">
 			              <input type="text" name="first_name" id="first_name" class="form-control medium-control " placeholder="First Name" tabindex="1">
@@ -58,4 +58,50 @@
 			</div>
 		</div>
 	</div>
-</div> 
+</div>  -->
+
+
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="margin-bottom:2px;">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="[[URL::to('assets/images/slider1.jpg')]]" alt="..." style="height:500px !important; width:100% !important;">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="[[URL::to('assets/images/slider2.jpg')]]" alt="..." style="height:500px !important; width:100% !important;">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+     <div class="item">
+      <img src="[[URL::to('assets/images/slider3.jpg')]]" alt="..." style="height:500px !important; width:100% !important;">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"style="top:40% !important;">
+    <span class="fa fa-angle-left" aria-hidden="true"></span>
+    <!-- <i class="" aria-hidden="true"></i> -->
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" style="top:40% !important;">
+    <span class="fa fa-angle-right" aria-hidden="true"></span>
+    <!-- <i class="fa fa-angle-right" aria-hidden="true"></i> -->
+    <span class="sr-only">Next</span>
+  </a>
+</div>
