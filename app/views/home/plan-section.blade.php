79d35523443fcb9cdee73017f96cd083395934e8
sudiptpa <sudiptpa@gmail.com>
2014-11-26 23:09:14 +0545

    type changes

--- app/views/home/plan-section.blade.php
+++ app/views/home/plan-section.blade.php
@@ -27,7 +27,7 @@
                 </div> 
                 <div class="panel-footer">
                     [[Form::open(array('url'=>'register?subscription=free','method'=>'GET'))]]
-                        <input type="hidden" name="planType" value="0">
+                        <input type="hidden" name="plan" value="0">
                         <button type="submit" class="btn btn-default btn-default-plan free-plan">Get Started</button>
                     [[Form::close()]]
 				</div>                               
@@ -59,7 +59,7 @@
                 </div> 
                 <div class="panel-footer">
                     [[Form::open(array('url'=>'register/now?subscription=premium','method'=>'GET'))]]
-                        <input type="hidden" name="planType" value="1">
+                        <input type="hidden" name="plan" value="1">
                         <button type="submit" class="btn-green btn-normal silver-plan"><strong>Get Started</strong></button>
                     [[Form::close()]]
 				</div>
@@ -87,7 +87,7 @@
                 </div>
                  <div class="panel-footer">
                     [[Form::open(array('url'=>'register/now?subscription=premium','method'=>'GET'))]]
-                        <input type="hidden" name="planType" value="2">
+                        <input type="hidden" name="plan" value="2">
                         <button type="submit" class="btn btn-gold btn-normal gold-plan"><strong>Get Started</strong></button>
                     [[Form::close()]]
 				</div>
@@ -114,7 +114,7 @@
                 </div>
                 <div class="panel-footer">                    
                     [[Form::open(array('url'=>'register/now?subscription=premium','method'=>'GET'))]]
-                        <input type="hidden" name="planType" value="3">
+                        <input type="hidden" name="plan" value="3">
                         <button type="submit" class="btn btn-sky btn-normal platinum-plan"><strong>Get Started</strong></button>
                     [[Form::close()]]
                 </div>
